20e406b4aaa455ec21c11b3f610f3055d7a62fc9
mstssk <mstssk+github@gmail.com>
2014-10-04 16:17:34 +0900

    Initial commit

---
+++ app/src/main/java/jp/mstssk/mtr/mobiletw_redirection/Redirection.java
@@ -0,0 +1,23 @@
+package jp.mstssk.mtr.mobiletw_redirection;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.Toast;
+
+
+public class Redirection extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        String url = getIntent().getData().toString();
+        String newUrl = url.replaceFirst("mobile.", "");
+        Toast.makeText(getApplicationContext(), newUrl, Toast.LENGTH_SHORT).show();
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(newUrl)));
+
+        finish();
+    }
+}
